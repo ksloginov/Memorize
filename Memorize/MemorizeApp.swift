8f2a227c855c082ac512e02ab98b18caae37bf03
Konstantin Loginov <loginov.k@gmail.com>
2021-07-23 19:32:13 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Konstantin Loginov on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
