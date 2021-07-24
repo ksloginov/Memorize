24ae47ac9e41f0249f13d4da24d8d9108ae8faff
Konstantin Loginov <loginov.k@gmail.com>
2021-07-24 11:38:16 +0200

    Step 56: Refactoring indexOf to return optional

--- Memorize/MemoryGameModel.swift
+++ Memorize/MemoryGameModel.swift
@@ -11,18 +11,19 @@ struct MemoryGameModel<CardContent> {
     private (set) var cards: Array<Card>
     
     mutating func choose(card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
-    func index(of card: Card) -> Int {
+    func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id {
                 return index
             }
         }
         
-        return 0 // TODO: Fix it later? It doesn't have to return 0!
+        return nil
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
