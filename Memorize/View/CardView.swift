98ed90f1f14159a171e282e9dbc8a46ad18993a1
Konstantin Loginov <loginov.k@gmail.com>
2021-07-24 12:28:22 +0200

    Step 68: Cleaning CardView code w/ DrawingConstants

--- Memorize/View/CardView.swift
+++ Memorize/View/CardView.swift
@@ -14,23 +14,29 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: 20)
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
                 if card.isFaceUp {
                     shape
                         .fill()
                         .foregroundColor(.white)
                     shape
-                        .stroke(lineWidth: 3.0)
+                        .stroke(lineWidth: DrawingConstants.lineWidth)
                     Text(card.content)
-                        .font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.7))
+                        .font(Font.system(size: min(geometry.size.width, geometry.size.height) * DrawingConstants.fontScale))
                 } else if card.isMatched {
-                    shape.opacity(0)
+                    shape.opacity(0.0)
                 } else {
                     shape
                         .fill()
                 }
             }
         }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20.0
+        static let lineWidth: CGFloat = 3.0
+        static let fontScale: CGFloat = 0.7
     }
 }
 
