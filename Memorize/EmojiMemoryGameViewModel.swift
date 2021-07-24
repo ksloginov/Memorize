e9c309d4b2e71cdec4da1b17f7ec6145acb3f538
Konstantin Loginov <loginov.k@gmail.com>
2021-07-24 11:02:21 +0200

    Step 40: Initializing the model inside the ViewModel

--- Memorize/EmojiMemoryGameViewModel.swift
+++ Memorize/EmojiMemoryGameViewModel.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
+func makeCardContent(index: Int) -> String {
+    return "🦄"
+}
+
 class EmojiMemoryGameViewModel {
-    private var model: MemoryGameModel<String>
+    private var model: MemoryGameModel<String> = MemoryGameModel<String>(numberOfPairsOfCards: 4, createCardContent: makeCardContent)
     
     var cards: Array<MemoryGameModel<String>.Card> {
         return model.cards
