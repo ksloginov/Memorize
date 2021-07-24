f6a94efe6d345ae23176ef3f1659e0b6bad60173
Konstantin Loginov <loginov.k@gmail.com>
2021-07-24 10:51:16 +0200

    Step 34: Adding a Model struct to the project

---
+++ Memorize/MemoryGameModel.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryGameModel.swift
+//  Memorize
+//
+//  Created by Konstantin Loginov on 24.07.2021.
+//
+
+import Foundation
+
+struct MemoryGameModel {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+}
