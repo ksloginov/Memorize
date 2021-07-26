4b3f3dcf9296fb7164c8343b4b5387aec20b0789
Konstantin Loginov <loginov.k@gmail.com>
2021-07-26 16:12:22 +0200

    [⭐️] Step 92: Adding a copy-pasted FootballMemoryGameView & ViewModel for it

--- Memorize/View/GamesListView.swift
+++ Memorize/View/GamesListView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 struct GamesListview: View {
     
     private let emojiGameViewModel = EmojiMemoryGameViewModel()
+    private let footballGameViewModel = FootballGameViewModel()
     
     var body: some View {
         List {
             NavigationLink(destination: EmojiMemoryGameView(viewModel: emojiGameViewModel)) {
                 Text("Emoji game 🤖")
+                    .font(.largeTitle)
+                    .padding(.vertical)
+            }
+            
+            NavigationLink(destination: FootballMemoryGameView(viewModel: footballGameViewModel)) {
+                Text("Football game ⚽️")
                     .font(.largeTitle)
                     .padding(.vertical)
             }
